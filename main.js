7df600cd2647d9fbc5ef7232a859a697d0939088
Dacen12 <alkisalki1@hotmail.com>
2022-02-14 20:31:06 +0100

    first commit backend

---
+++ main.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const qrcode = require('./qrcode')
+const fs = require('fs')
+const cors = require('cors')
+const path= require('path')
+
+
+app.use(cors())
+app.use(express.json())
+
+
+app.post('/getQR', async (req,res) => {
+    res.set('Access-Control-Allow-Origin', '*')   
+const url = await qrcode(req.body)
+res.send(url) 
+})
+
+
+
+app.get('/download', (req,res) => {
+    
+    res.sendFile(req.body.imgUrl)
+})
+
+
+app.get('showImage', (req, res) => {
+res.sendFile('./temp/ige.png')
+})
+
+app.listen(4999 ,() =>{
+console.log('server is running')
+})
---
+++ qrcode.js
@@ -0,0 +1,96 @@
+const qrcode = require('qrcode')
+const path = require('path')
+const vCard = require('vcards-js')
+const card = vCard()
+const fs = require('fs')
+const cloud = require('./storage')
+
+
+// generates image, params = input fields & component identity
+ async function generate (params) { 
+    const {type, input} = params
+   
+   const message = converter(type, input)
+
+   const id = makeId(7)
+   const url = path.join(__dirname, `/temp/QR_${id}.png`)
+
+   await qrcode.toFile(url, message)
+
+  const imageResponse = await cloud.uploader.upload(url)
+  
+  return {url: imageResponse.secure_url, id: imageResponse.public_id}
+}
+   
+   
+
+
+
+
+const converter = (type, input) => {
+    try{
+
+        switch(type){
+            case 'vcard':
+                return vCardGenerator(input)
+            case 'message':
+                return input.message
+            case 'mail':
+                return emailQR(input)
+            case 'sms':
+                return mobileQR(input)
+            default: 
+            // none
+        }
+
+    } catch(err){
+        if(err) console.log(`err while trying to convert : ${err}`)
+    }
+    
+}
+
+
+
+// vcard
+const vCardGenerator = (input) => {
+    card.firstName = input.f_name
+    card.lastName = input.s_name
+    card.email = input.email
+    card.cellPhone = input.number
+    card.organization = input.company
+    card.homeAddress.street = input.street
+    card.homeAddress.city = input.residence
+    card.homeAddress.postalCode = input.zip
+    card.url = `https://${input.website}`
+    
+    return card.getFormattedString()
+}
+
+
+// email :
+
+const emailQR = (params) => {
+    const {email, subject} = params
+    return `mailto:${email}?subject=${subject}`
+}
+
+//phone 
+const mobileQR = (params) => {
+    const {number, body} = params
+    return `sms:${number}?body=${body}`
+}
+
+function makeId (length) {
+    var result           = '';
+    var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < length; i++ ) {
+      result += characters.charAt(Math.floor(Math.random() * 
+ charactersLength));
+   }
+   return result;
+}
+
+module.exports = async (params) => {
+    return await generate(params)
+}
